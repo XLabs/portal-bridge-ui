70f77dfb926a57a23121f032474781a95811f7a4
Valentino Conti <valeen.conti@hotmail.com>
2023-05-11 19:07:42 -0300

    cluster fix

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -72,12 +72,12 @@ import {
 } from "@mysten/sui.js";
 
 export type Cluster = "devnet" | "testnet" | "mainnet";
-export const CLUSTER: Cluster = "testnet" as Cluster;
-// process.env.REACT_APP_CLUSTER === "mainnet"
-//   ? "mainnet"
-//   : process.env.REACT_APP_CLUSTER === "testnet"
-//   ? "testnet"
-//   : "devnet";
+export const CLUSTER: Cluster =
+  process.env.REACT_APP_CLUSTER === "mainnet"
+    ? "mainnet"
+    : process.env.REACT_APP_CLUSTER === "testnet"
+    ? "testnet"
+    : "devnet";
 
 export interface ChainInfo {
   id: ChainId;
