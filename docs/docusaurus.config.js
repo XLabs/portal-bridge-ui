214071cebcad7975a6141b173c282b8ecacc97f0
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-06-17 16:19:38 -0300

    set base url to PUBLIC_URL env

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://www.portalbridge.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs",
+  baseUrl: process.env.PUBLIC_URL || "/docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
