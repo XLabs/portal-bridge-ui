a9456c7dbc4589d03c9696be9fbdfc1942fcda6a
Valentino Conti <valeen.conti@hotmail.com>
2023-07-29 16:44:02 -0300

    hotfix for sanctionAddress lambda

--- src/utils/sanctions.ts
+++ src/utils/sanctions.ts
@@ -24,70 +24,74 @@ export const getIsSanctioned = async (
 ) => {
   if (!addr) return;
 
-  const trmChain = getTrmChainName(chainId);
-  const localStorageKey = `${trmChain}-${addr}-1`;
-  const rightNow = new Date();
-
-  let storedResult = "";
-  const storedValue = localStorage.getItem(localStorageKey);
-
-  if (storedValue) {
-    const stored = JSON.parse(storedValue);
-
-    if (new Date(stored.expires) < rightNow) {
-      localStorage.removeItem(localStorageKey);
-    } else {
-      storedResult = stored.isSanctioned;
-    }
-  }
-
-  if (storedResult !== "") return storedResult;
-
-  if (trmChain && CLUSTER === "mainnet") {
-    let isSanctioned = false;
-
-    const resp = await fetch(
-      "https://hjukqn406c.execute-api.us-east-2.amazonaws.com/addresses",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify([
-          {
-            // address: "149w62rY42aZBox8fGcmqNsXUzSStKeq8C", // sanctioned address example
-            // chain: "bitcoin", // sanctioned address example
-            address: addr,
-            chain: trmChain,
-            accountExternalId: "PortalBridge",
-          },
-        ]),
-      }
-    );
-
-    const data = await resp.json();
-    const screeningData = data[0] as SanctionResponse;
-
-    screeningData.addressRiskIndicators.forEach((risk) => {
-      if (risk.categoryRiskScoreLevel >= 10 && risk.riskType === "OWNERSHIP") {
-        isSanctioned = true;
-      }
-    });
-
-    screeningData.entities.forEach((entity) => {
-      if (entity.riskScoreLevel >= 10) {
-        isSanctioned = true;
-      }
-    });
-
-    // store result on localStorage for one week
-    localStorage.setItem(
-      localStorageKey,
-      JSON.stringify({
-        expires: new Date(rightNow.getTime() + 7 * 24 * 60 * 60 * 1000),
-        isSanctioned: isSanctioned,
-      })
-    );
-
-    return isSanctioned;
-  }
   return false;
+
+  // TODO: Uncomment this all when lambda function for sanctioned address get fixed
+
+  // const trmChain = getTrmChainName(chainId);
+  // const localStorageKey = `${trmChain}-${addr}-1`;
+  // const rightNow = new Date();
+
+  // let storedResult = "";
+  // const storedValue = localStorage.getItem(localStorageKey);
+
+  // if (storedValue) {
+  //   const stored = JSON.parse(storedValue);
+
+  //   if (new Date(stored.expires) < rightNow) {
+  //     localStorage.removeItem(localStorageKey);
+  //   } else {
+  //     storedResult = stored.isSanctioned;
+  //   }
+  // }
+
+  // if (storedResult !== "") return storedResult;
+
+  // if (trmChain && CLUSTER === "mainnet") {
+  //   let isSanctioned = false;
+
+  //   const resp = await fetch(
+  //     "https://hjukqn406c.execute-api.us-east-2.amazonaws.com/addresses",
+  //     {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify([
+  //         {
+  //           // address: "149w62rY42aZBox8fGcmqNsXUzSStKeq8C", // sanctioned address example
+  //           // chain: "bitcoin", // sanctioned address example
+  //           address: addr,
+  //           chain: trmChain,
+  //           accountExternalId: "PortalBridge",
+  //         },
+  //       ]),
+  //     }
+  //   );
+
+  //   const data = await resp.json();
+  //   const screeningData = data[0] as SanctionResponse;
+
+  //   screeningData.addressRiskIndicators.forEach((risk) => {
+  //     if (risk.categoryRiskScoreLevel >= 10 && risk.riskType === "OWNERSHIP") {
+  //       isSanctioned = true;
+  //     }
+  //   });
+
+  //   screeningData.entities.forEach((entity) => {
+  //     if (entity.riskScoreLevel >= 10) {
+  //       isSanctioned = true;
+  //     }
+  //   });
+
+  //   // store result on localStorage for one week
+  //   localStorage.setItem(
+  //     localStorageKey,
+  //     JSON.stringify({
+  //       expires: new Date(rightNow.getTime() + 7 * 24 * 60 * 60 * 1000),
+  //       isSanctioned: isSanctioned,
+  //     })
+  //   );
+
+  //   return isSanctioned;
+  // }
+  // return false;
 };
