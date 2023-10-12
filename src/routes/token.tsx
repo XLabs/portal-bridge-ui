40fb58c018e2c1c0645f4f5848f091309608034e
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-10-12 17:29:33 -0300

    add missing prop

--- src/routes/token.tsx
+++ src/routes/token.tsx
@@ -24,6 +24,7 @@ const config: WormholeConnectConfig = {
       },
       {
         icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch' focusable='false' aria-hidden='true' viewBox='0 0 24 24' data-testid='OpenInNewIcon'%3E%3Cpath d='M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z' fill='%23ffffff'%3E%3C/path%3E%3C/svg%3E",
+        name: 'more',
         label: 'More ...',
         href: 'https://portalbridge.com/#/transfer',
         showOpenInNewIcon: false,
