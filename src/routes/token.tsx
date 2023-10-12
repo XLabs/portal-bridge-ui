d094dff140e1c4facf4fe02ebbff498dd6acd956
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-10-12 17:01:58 -0300

    setup config example

--- src/routes/token.tsx
+++ src/routes/token.tsx
@@ -9,7 +9,7 @@ const config: WormholeConnectConfig = {
   env: import.meta.env.VITE_APP_CLUSTER || "mainnet",
   pageHeader: "Token Bridge",
   pageSubHeader: "Portal is a bridge that offers unlimited transfers across chains for tokens and NFTs wrapped by Wormhole. Unlike many other bridges, you avoid double wrapping and never have to retrace your steps.",
-
+  
   moreTokens: {
     href: 'https://portalbridge.com?sourceChain={:sourceChain}&targetChain={:targetChain}',
     label: 'More tokens ...',
@@ -19,18 +19,18 @@ const config: WormholeConnectConfig = {
     networks: [
       {
         name: 'algorand',
-        icon: "data:image/svg+xml,%3Csvg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 650 650'%3E%3Cdefs%3E%3Cstyle%3E.cls-1%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ctitle%3EALGO_Logos_190320%3C/title%3E%3Cg id='lINT7W'%3E%3Cpolygon class='cls-1' points='444.18 444.32 406.81 444.32 382.54 354.04 330.36 444.33 288.64 444.33 369.29 304.57 356.31 256.05 247.56 444.36 205.82 444.36 343.64 205.64 380.18 205.64 396.18 264.95 433.88 264.95 408.14 309.71 444.18 444.32'/%3E%3C/g%3E%3C/svg%3E",
+        icon: "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3Csvg viewBox='532.3494 45.7309 238.36 238.72' width='238.36' height='238.72' xmlns='http://www.w3.org/2000/svg'%3E%3Cg id='lINT7W' transform='matrix(1, 0, 0, 1, 326.5293884277344, -159.9090576171875)'%3E%3Cpolygon class='cls-1' points='444.18 444.32 406.81 444.32 382.54 354.04 330.36 444.33 288.64 444.33 369.29 304.57 356.31 256.05 247.56 444.36 205.82 444.36 343.64 205.64 380.18 205.64 396.18 264.95 433.88 264.95 408.14 309.71 444.18 444.32' style='fill: rgb(255, 255, 255);'/%3E%3C/g%3E%3C/svg%3E",
         label: 'Algorand',
       },
       {
         icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch' focusable='false' aria-hidden='true' viewBox='0 0 24 24' data-testid='OpenInNewIcon'%3E%3Cpath d='M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z' fill='%23ffffff'%3E%3C/path%3E%3C/svg%3E",
         label: 'More ...',
         href: 'https://portalbridge.com/#/transfer',
-        showOpenInNewIcon: false
-      }
-    ]
-  }
-  
+        showOpenInNewIcon: false,
+      },
+    ],
+  },
+
 };
 
 export default function TokenBridge() {
