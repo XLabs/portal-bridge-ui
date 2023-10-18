f2499d46afb13507adce960aea0ce04525cfd630
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-10-18 13:04:41 -0300

    inject advance path, fix version

--- apps/connect/src/routes/token.tsx
+++ apps/connect/src/routes/token.tsx
@@ -10,11 +10,11 @@ const config: WormholeConnectConfig = {
   pageHeader: "Token Bridge",
   pageSubHeader: "Portal is a bridge that offers unlimited transfers across chains for tokens and NFTs wrapped by Wormhole. Unlike many other bridges, you avoid double wrapping and never have to retrace your steps.",
   moreTokens: {
-    href: 'https://portalbridge.com/#/transfer?sourceChain={:sourceChain}&targetChain={:targetChain}',
+    href: `${import.meta.env.VITE_APP_ADVANCE_PATH}/#/transfer?sourceChain={:sourceChain}&targetChain={:targetChain}`,
     label: 'More tokens ...',
   },
   extraNetworks: {
-    href: 'https://portalbridge.com/#/transfer?sourceChain={:sourceChain}&targetChain={:targetChain}',
+    href: `${import.meta.env.VITE_APP_ADVANCE_PATH}/#/transfer?sourceChain={:sourceChain}&targetChain={:targetChain}`,
     description: 'Portal is a bridge that offers unlimited transfers across chains for tokens and NFTs wrapped by Wormhole. Unlike many other bridges, you avoid double wrapping and never have to retrace your steps.',
     networks: [
       {
@@ -26,7 +26,7 @@ const config: WormholeConnectConfig = {
         icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch' focusable='false' aria-hidden='true' viewBox='0 0 24 24' data-testid='OpenInNewIcon'%3E%3Cpath d='M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z' fill='%23ffffff'%3E%3C/path%3E%3C/svg%3E",
         name: 'more',
         label: 'More ...',
-        href: 'https://portalbridge.com/#/transfer',
+        href: `${import.meta.env.VITE_APP_ADVANCE_PATH}/#/transfer`,
         showOpenInNewIcon: false,
       },
     ],
