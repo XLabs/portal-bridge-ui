9eb51608e252ef3c724f81ef87b2f0890cf1740d
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-11-20 16:07:06 -0300

    #523 - add more networks and more tokens settings

--- apps/connect/vite.usdc-bridge.config.ts
+++ apps/connect/vite.usdc-bridge.config.ts
@@ -4,6 +4,7 @@ import viteConfig from './vite.config'
 // https://vitejs.dev/config/
 export default defineConfig({
   ...viteConfig,
+  base: `${viteConfig.base}usdc-bridge/`,
   define: {
     wormholeConnectConfig: {
       ...viteConfig?.define?.wormholeConnectConfig,
