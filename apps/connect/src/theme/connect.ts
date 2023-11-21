89001417387bf780f50dd404ebbef2391f3ed488
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-11-21 15:26:19 -0300

    update pallete as design

--- apps/connect/src/theme/connect.ts
+++ apps/connect/src/theme/connect.ts
@@ -1,58 +1,139 @@
-import {
-    Theme,
-    OPACITY
-} from "@wormhole-foundation/wormhole-connect";
-import lightblue from "@mui/material/colors/lightBlue";
-import grey from "@mui/material/colors/grey";
-import green from "@mui/material/colors/green";
-import orange from "@mui/material/colors/orange";
-import red from "@mui/material/colors/red";
-
+import type { Theme } from "@wormhole-foundation/wormhole-connect";
 const customized: Theme = {
-    divider: "#ffffff" + OPACITY[20],
+    primary: {
+        50: "#fafafa",
+        100: "#f5f5f5",
+        200: "#eeeeee",
+        300: "#e0e0e0",
+        400: "#bdbdbd",
+        500: "#9e9e9e",
+        600: "#757575",
+        700: "#616161",
+        800: "#424242",
+        900: "#212121",
+        A100: "#f5f5f5",
+        A200: "#eeeeee",
+        A400: "#bdbdbd",
+        A700: "#616161"
+    },
+    secondary: {
+        50: "#fafafa",
+        100: "#f5f5f5",
+        200: "#eeeeee",
+        300: "#e0e0e0",
+        400: "#bdbdbd",
+        500: "#9e9e9e",
+        600: "#757575",
+        700: "#616161",
+        800: "#424242",
+        900: "#212121",
+        A100: "#f5f5f5",
+        A200: "#eeeeee",
+        A400: "#bdbdbd",
+        A700: "#616161"
+    },
+    divider: "#ffffff33",
     background: {
-        default: "transparent",
+        default: "rgba(12, 9, 60, 0)"
     },
     text: {
-        primary: "#ffffff",
-        secondary: grey[500],
+        primary: "rgba(255, 255, 255, 0.9)",
+        secondary: "rgba(156, 157, 191, 1)"
+    },
+    error: {
+        50: "#ffebee",
+        100: "#ffcdd2",
+        200: "#ef9a9a",
+        300: "#e57373",
+        400: "#ef5350",
+        500: "#f44336",
+        600: "#e53935",
+        700: "#d32f2f",
+        800: "#c62828",
+        900: "#b71c1c",
+        A100: "#ff8a80",
+        A200: "#ff5252",
+        A400: "#ff1744",
+        A700: "#d50000"
+    },
+    info: {
+        50: "#97a5b7",
+        100: "#8293a9",
+        200: "#6e819a",
+        300: "#596f8c",
+        400: "#445d7e",
+        500: "#304C70",
+        600: "#2b4464",
+        700: "#263c59",
+        800: "#21354e",
+        900: "#1c2d43",
+        A100: "#304C70",
+        A200: "#304C70",
+        A400: "#304C70",
+        A700: "#304C70"
+    },
+    success: {
+        50: "#66d6cd",
+        100: "#4dcfc4",
+        200: "#33c8bc",
+        300: "#1ac1b4",
+        400: "#01BBAC",
+        500: "#00a89a",
+        600: "#009589",
+        700: "#008278",
+        800: "#007067",
+        900: "#005d56",
+        A100: "#00a89a",
+        A200: "#00a89a",
+        A400: "#00a89a",
+        A700: "#00a89a"
+    },
+    warning: {
+        50: "#ffe3a4",
+        100: "#ffdd91",
+        200: "#ffd77f",
+        300: "#ffd26d",
+        400: "#ffcc5b",
+        500: "#FFC749",
+        600: "#e5b341",
+        700: "#cc9f3a",
+        800: "#b28b33",
+        900: "#99772b",
+        A100: "#FFC749",
+        A200: "#FFC749",
+        A400: "#FFC749",
+        A700: "#FFC749"
     },
     button: {
-        primary: "#ffffff19",
+        primary: "rgba(49, 50, 102, 1)",
         primaryText: "#ffffff",
         disabled: "#ffffff0F",
         disabledText: "#ffffff66",
-        action: "#ffffff",
-        actionText: "#17153F",
-        hover: "#ffffff0F",
-    },
-    font: {
-        primary: "Poppins, sans-serif",
-        header: "Suisse BP Intl, sans-serif",
+        action: "rgba(74, 75, 147, 1)",
+        actionText: "#000000",
+        hover: "#ffffff0F"
     },
     options: {
-        hover: "rgb(15, 16, 36);",
-        select: "#5b5b5b",
+        hover: "#ffffff0F",
+        select: "#ffffff19"
     },
     card: {
-        background: "rgba(255, 255, 255, 0.047);",
-        secondary: "rgba(255, 255, 255, 0.047);",
-        elevation: "none",
+        background: "rgba(31, 32, 69, 1)",
+        secondary: "#ffffff0C",
+        elevation: "none"
     },
     popover: {
         background: "#1b2033",
-        secondary: "#ffffff" + OPACITY[5],
-        elevation: "none",
+        secondary: "#ffffff0C",
+        elevation: "none"
     },
     modal: {
-        background: "rgb(15, 16, 36, 0.95);",
+        background: "#0F092C"
     },
-    primary: grey,
-    secondary: grey,
-    error: red,
-    info: lightblue,
-    success: green,
-    warning: orange,
+    font: {
+        primary: "\"Poppins\", regular",
+        header: "\"Poppins\", bold",
+    }
 };
 
 export default customized;
