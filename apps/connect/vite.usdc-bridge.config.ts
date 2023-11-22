e4d1e2615a58e827d12a7c3d7c8c67ff004b3af3
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-11-22 19:13:03 -0300

    fix testnet networks

--- apps/connect/vite.usdc-bridge.config.ts
+++ apps/connect/vite.usdc-bridge.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import viteConfig from './vite.config'
 
 const PUBLIC_URL = viteConfig.base;
+const TESTNET_NETWORKS = ['goerli', 'fuji', 'arbitrumgoerli', 'optimismgoerli', 'basegoerli'];
+const MAINNET_NETWORKS = ["ethereum", "avalanche", "arbitrum", "optimism", "base"];
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +18,7 @@ export default defineConfig({
       ...viteConfig?.define?.wormholeConnectConfig,
       pageHeader: 'USDC Bridge',
       routes: ["cctpManual", "cctpRelay"],
-      networks: ["ethereum", "avalanche", "arbitrum", "optimism", "base"],
+      networks: process.env.VITE_APP_CLUSTER === 'mainnet' ? MAINNET_NETWORKS : TESTNET_NETWORKS,
       tokens: ["USDCeth", "USDCavax", "USDCarbitrum", "USDCoptimism", "USDCbase"],
     }
   }
