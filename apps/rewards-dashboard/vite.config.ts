3f0eb8b9e346edf1c9c8c962dc707173ac122691
a <a@tuxpa.in>
2023-12-20 23:47:52 -0600

    back

--- apps/rewards-dashboard/vite.config.ts
+++ apps/rewards-dashboard/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import  { resolve } from 'path'
 const PUBLIC_URL = process.env.PUBLIC_URL || "/rewards-dashboard"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: PUBLIC_URL,
@@ -9,4 +10,11 @@ export default defineConfig({
     "BASE_URL": `"${PUBLIC_URL}"`,
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
