8afd210dc8ce5f539a5c6cd9bb00fe92949db26f
a <a@tuxpa.in>
2023-12-29 16:05:21 -0600

    new

--- apps/rewards-dashboard/src/providers/index.tsx
+++ apps/rewards-dashboard/src/providers/index.tsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LocaleProvider } from "./LocaleProvider";
 import { Web3Provider } from "./Web3Provider";
+
+const queryClient = new QueryClient()
 
 export const AppProvider = (props: { children: JSX.Element }) => {
   return (
     <>
       <LocaleProvider>
-        <Web3Provider>{props.children}</Web3Provider>
+        <QueryClientProvider client={queryClient}>
+          <Web3Provider>
+            {props.children}
+          </Web3Provider>
+        </QueryClientProvider>
       </LocaleProvider>
     </>
   );
