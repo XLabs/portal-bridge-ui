88aa76cae9223f51c746d1753136fdd8eea31b34
a <a@tuxpa.in>
2024-04-01 14:25:54 -0500

    changes

--- apps/rewards-dashboard/src/App.tsx
+++ apps/rewards-dashboard/src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { DashboardLayout } from "./layouts/DashboardLayout";
 import { AppProvider } from "./providers";
 import { Trans } from "@lingui/macro";
+import { RxCross1 } from "react-icons/rx";
 
 function App() {
   const [hideModal, setHideModal] = useState(false);
@@ -18,19 +19,24 @@ function App() {
           ) : (
             <div className="absolute flex flex-col grow h-full w-full scrollbar-hide justify-center items-center ">
               <div className="w-96 h-40 bg-[#262b5a] z-10 rounded-3xl border border-1 border-gray-500">
-                <div className="flex flex-col">
-                  <div className="flex flex-row justify-end text-gray-300 pr-4 pt-1">
-                    <a
-                      className="hover:cursor-pointer"
-                      onClick={() => {
-                        setHideModal(true);
-                      }}
-                    >
-                      x
-                    </a>
+                <div className="flex flex-col gap-4">
+                  <div className="flex flex-row justify-between text-gray-300 py-1 justify-center border-b border-gray-500 ">
+                    <div className= "flex flex-row items-center grow justify-center text-white">
+                        Program Ended
+                    </div>
+                    <div className="flex flex-row items-center justify-center">
+                      <a
+                        className="hover:cursor-pointer pr-3"
+                        onClick={() => {
+                          setHideModal(true);
+                        }}
+                      >
+                        <RxCross1 />
+                      </a>
+                    </div>
                   </div>
-                  <div className="flex flex-row justify-start text-white text-sm pt-2">
-                    <p className="mx-4 align-start text-left whitespace-pre">
+                  <div className="flex flex-row justify-start text-white text-sm">
+                    <p className="align-start w-full text-center whitespace-pre">
                       <Trans>Rewards Ended Information</Trans>
                     </p>
                   </div>
--- apps/rewards-dashboard/src/constants.ts
+++ apps/rewards-dashboard/src/constants.ts
@@ -1,3 +1,3 @@
 export const WAC_URL = import.meta.env.VITE_WAC_URL
   ? import.meta.env.VITE_WAC_URL
-  : "https://wac.staging.gfx.town";
+  : "https://wac.gfx.xyz";
