8d0aec43b4e43da08898a2a7ef88a2c7e82d757a
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2024-04-15 15:35:53 -0300

    xlabs/portal-bridge-ui-issues#61 - prettier

--- apps/connect/src/vite-env.d.ts
+++ apps/connect/src/vite-env.d.ts
@@ -22,5 +22,5 @@ declare global {
   declare const wormholeConnectConfig: WormholeConnectConfig;
   declare const navBar: NavLink[];
   declare const redirects: Redirect;
-  declare const versions: Version[]
+  declare const versions: Version[];
 }
