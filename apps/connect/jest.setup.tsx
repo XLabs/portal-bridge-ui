5706125d9d96b2212f0ca01dda49bf1c204a1d8c
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2024-07-12 13:23:43 +0200

    linter fix

--- apps/connect/jest.setup.tsx
+++ apps/connect/jest.setup.tsx
@@ -2,7 +2,9 @@ import "@testing-library/jest-dom";
 
 import { TextEncoder, TextDecoder } from "util";
 
-const navBar: any[] = [];
-const wormholeConnectConfig: any = {};
-
-Object.assign(global, { TextDecoder, TextEncoder, navBar, wormholeConnectConfig });
+Object.assign(global, {
+  TextDecoder,
+  TextEncoder,
+  navBar: [],
+  wormholeConnectConfig: {},
+});
