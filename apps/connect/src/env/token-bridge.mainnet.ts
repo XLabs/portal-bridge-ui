cb15d691add60dba3a973b10e6ecd84b2c5dbe98
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2024-08-22 11:54:02 -0300

    add RENZO quoter for solana

--- apps/connect/src/env/token-bridge.mainnet.ts
+++ apps/connect/src/env/token-bridge.mainnet.ts
@@ -1041,6 +1041,7 @@ export const ENV: Env = {
                   type: "wormhole",
                 },
               ],
+              solanaQuoter: "Nqd6XqA8LbsCuG8MLWWuP865NV6jR1MbXeKxD4HLKDJ",
             },
           ],
         },
