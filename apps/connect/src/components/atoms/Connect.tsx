815a747c4d61a6009cce8f375b9d5e2c171291ac
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2024-09-06 11:45:06 +0200

    rebranding updates

--- apps/connect/src/components/atoms/Connect.tsx
+++ apps/connect/src/components/atoms/Connect.tsx
@@ -1,8 +1,17 @@
-import { memo, useEffect } from "react";
-import WormholeConnect from "@wormhole-foundation/wormhole-connect";
+import { ComponentProps, memo, useEffect } from "react";
+import WormholeConnect from "@wormhole-foundation/wormhole-connect-v2";
 import { theme } from "../../theme/connect";
 import Banner from "./Banner";
 import { useConnectConfig } from "../../hooks/useConnectConfig";
+import { styled } from "@mui/material";
+import { NAVBAR_WIDTH } from "./NavBar";
+
+const Container = styled("div")(({ theme }) => ({
+  paddingRight: `${NAVBAR_WIDTH}px`,
+  [theme.breakpoints.down("md")]: {
+    paddingRight: 0,
+  },
+}));
 
 export const Connect = memo(() => {
   const config = useConnectConfig();
@@ -14,9 +23,22 @@ export const Connect = memo(() => {
   }, [config]);
 
   return (
-    <>
-      {!!config && <WormholeConnect config={config} theme={theme} />}
+    <Container>
+      {!!config && (
+        <WormholeConnect
+          config={
+            {
+              useRedesign: true,
+              rpcs: {
+                Solana:
+                  "https://solana-mainnet.g.alchemy.com/v2/57SZNfWzjQuYQ3U89jxghxXAbBT_JExa",
+              },
+            } as unknown as ComponentProps<typeof WormholeConnect>["config"]
+          }
+          theme={theme}
+        />
+      )}
       <Banner />
-    </>
+    </Container>
   );
 });
