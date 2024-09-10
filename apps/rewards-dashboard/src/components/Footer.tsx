e876ba971d364fad7579f1192e4ad5a6ba2e5052
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2024-09-10 11:03:04 +0200

    rewards header

--- apps/rewards-dashboard/src/components/Footer.tsx
+++ apps/rewards-dashboard/src/components/Footer.tsx
@@ -2,38 +2,21 @@ import { WormholeLogo } from "../quarks/LogoVectors";
 
 export const Footer = () => {
   return (
-    <>
-      <div
-        className="
-      flex flex-col mt-16 mb-4
-      gap-8
-      "
-      >
-        <div
-          className="
-        flex flex-row items-center justify-center
-        "
-        >
-          <div className="text-white text-xs text-opacity-60 pr-4">
-            Powered By
-          </div>
-          <WormholeLogo />
-        </div>
-        <div
-          className="
-        flex flex-row gap-4
-        flex-1
-        justify-between
-        "
-        >
-          <div className="text-white text-sm text-opacity-90">
-            v{import.meta.env.VITE_APP_VERSION || "0.0.0"}
-          </div>
-          <div className="text-white text-sm text-opacity-90">
-            Built by xLabs
-          </div>
-        </div>
+    <footer className="flex flex-row mt-16 gap-8 justify-between items-center">
+      <div className="text-white text-sm text-opacity-90">
+        v{import.meta.env.VITE_APP_VERSION || "0.0.0"}
       </div>
-    </>
+
+      <div className="flex flex-row items-center justify-center flex-1">
+        <div className="text-white text-xs text-opacity-60 pr-4">
+          Powered By
+        </div>
+        <WormholeLogo />
+      </div>
+
+      <div className=" flex flex-row gap-4 justify-between">
+        <div className="text-white text-sm text-opacity-90">Built by xLabs</div>
+      </div>
+    </footer>
   );
 };
