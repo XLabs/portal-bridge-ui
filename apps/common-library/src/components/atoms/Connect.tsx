41b40240d8f25a0bac6ad2365b3e707eb52a7d97
yuli-ferna <yuliferna123@gmail.com>
2024-09-11 14:53:49 -0400

    Delete common lib

--- apps/common-library/src/components/atoms/Connect.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "@mui/material";
-import { NAVBAR_WIDTH } from "./NavBar";
-
-export const Container = styled("div")(({ theme }) => ({
-  paddingRight: `${NAVBAR_WIDTH}px`,
-  [theme.breakpoints.down("md")]: {
-    paddingRight: 0,
-  },
-}));
--- apps/common-library/src/theme/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./connect";
-export * from "./connect-v1";
-export * from "./globalStyles";
-export * from "./portal";
