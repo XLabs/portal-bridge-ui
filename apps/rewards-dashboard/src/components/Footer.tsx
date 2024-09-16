aed948ce8dcf694e091df299a29cfddbce71e25e
yuli-ferna <yuliferna123@gmail.com>
2024-09-16 11:14:55 -0400

    Rm version in footer (Rewards)

--- apps/rewards-dashboard/src/components/Footer.tsx
+++ apps/rewards-dashboard/src/components/Footer.tsx
@@ -1,10 +1,19 @@
 import { WormholeLogo } from "../quarks/LogoVectors";
+import { NavItem } from "../quarks/NavItem";
 
 export const Footer = () => {
   return (
     <footer className="flex flex-row mt-16 gap-8 justify-between items-center">
       <div className="text-white text-sm text-opacity-90">
-        v{import.meta.env.VITE_APP_VERSION || "0.0.0"}
+        
+        {[
+          {
+            label: "Privacy Policy",
+            href: `${BASE_URL}/#/privacy-policy/`,
+          },
+        ].map(({ label, href }) => (
+          <NavItem item={{label, href}} />
+        ))}
       </div>
 
       <div className="flex flex-row items-center justify-center flex-1">
