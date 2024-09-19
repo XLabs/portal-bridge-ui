f25d227137ef87343da6f39b8831ad3e7c160ae8
yuli-ferna <yuliferna123@gmail.com>
2024-09-19 10:19:08 -0400

    Fix ntt manual route name

--- apps/connect/src/hooks/useConnectConfig.ts
+++ apps/connect/src/hooks/useConnectConfig.ts
@@ -32,7 +32,7 @@ const defaultConfig: WormholeConnectConfig = {
   isRouteSupportedHandler: async (td: any) => {
     // Disable manual NTT for Lido wstETHƒ
     return !(
-      td.route === "nttManual" &&
+      td.route === "ManualNtt" &&
       td.fromToken.tokenId !== "native" &&
       [
         "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
