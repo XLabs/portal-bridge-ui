177e69e7e350333dda81f1cf866269b6f2bf5fa6
yuli-ferna <35125931+yuli-ferna@users.noreply.github.com>
2025-01-13 09:25:40 -0400

    Update Connect 1.1.10, and Aptos WA update (#1228)

--- apps/connect/src/utils/isValidAddress.ts
+++ apps/connect/src/utils/isValidAddress.ts
@@ -1,23 +1,21 @@
 import base58 from "bs58";
 import * as ethers from "ethers";
-import { isCosmWasmChain, isEVMChain } from "@certusone/wormhole-sdk";
-import { AptosClient } from "aptos";
+import { AptosConfig, Aptos, Network } from "@aptos-labs/ts-sdk";
 import { bech32 } from "bech32";
 import { Chain } from "@wormhole-foundation/sdk";
-import { toChainNameFormat } from "./transferVerification";
+import { isEVMChain, isCosmWasmChain } from "./constants";
 
 export const isValidAddress = async (
   address: string,
   chain: Chain
 ): Promise<boolean> => {
   // TO DO: Add support for other evm chains with the new sdk
-  if (isEVMChain(toChainNameFormat(chain)) || chain === "Worldchain")
+  if (isEVMChain(chain) || chain === "Worldchain")
     return isValidEthereumAddress(address);
   if (chain === "Solana") return isValidSolanaAddress(address);
   if (chain === "Aptos") return isValidAptosAddress(address);
   if (chain === "Sui") return isValidSuiAddress(address);
-  if (isCosmWasmChain(toChainNameFormat(chain)))
-    return isValidCosmosAddress(address, chain);
+  if (isCosmWasmChain(chain)) return isValidCosmosAddress(address, chain);
 
   return false;
 };
@@ -49,10 +47,11 @@ const isValidSolanaAddress = (address: string): boolean => {
 };
 
 // Aptos Validation
-const aptosClient = new AptosClient("https://api.mainnet.aptoslabs.com/v1");
+const config = new AptosConfig({ network: Network.MAINNET });
+const aptos = new Aptos(config);
 const isValidAptosAddress = async (address: string) => {
   try {
-    return !!(await aptosClient.getAccount(address));
+    return !!(await aptos.getAccountInfo({ accountAddress: address }));
   } catch {
     return false;
   }
