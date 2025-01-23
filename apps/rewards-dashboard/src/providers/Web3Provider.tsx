90149751d83b101db4e7f10d73a1f068a624e726
Henry Han <Hanhenry88@gmail.com>
2025-01-23 12:23:07 +0800

    Remove comments

--- apps/rewards-dashboard/src/providers/Web3Provider.tsx
+++ apps/rewards-dashboard/src/providers/Web3Provider.tsx
@@ -4,8 +4,6 @@ import { optimism, mainnet } from "@reown/appkit/networks";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { WagmiProvider } from "wagmi";
 
-// const CHAINS_LIST = [optimism, mainnet];
-
 const envProjectId = import.meta.env.VITE_APP_WALLET_CONNECT_PROJECT_ID;
 const projectId = envProjectId ? envProjectId : "dummy-wc-key";
 
@@ -15,50 +13,11 @@ const metadata = {
   url: import.meta.env.VITE_APP_DOMAIN || "portalbridge.com",
   icons: [`https://portalbridge.com/favicon.ico`],
 };
-// const theseChains = [...CHAINS_LIST];
 
 const wagmiAdapter = new WagmiAdapter({
   networks: [mainnet, optimism],
   projectId,
 });
-
-// const { chains, publicClient, webSocketPublicClient } = configureChains(
-//   theseChains,
-//   [
-//     jsonRpcProvider({
-//       rpc: (chain) => {
-//         const a = {
-//           [optimism.id]: {
-//             http: "https://rpc.ankr.com/optimism",
-//           },
-//         }[chain.id];
-//         return a ? a : null;
-//       },
-//     }),
-//     walletConnectProvider({
-//       projectId,
-//     }),
-//     publicProvider(),
-//   ],
-//   { pollingInterval: 10_000 }
-// );
-
-// const wagmiConfig = createConfig({
-//   autoConnect: true,
-//   connectors: [
-//     new WalletConnectConnector({
-//       chains,
-//       options: { projectId, showQrModal: false, metadata },
-//     }),
-//     new InjectedConnector({ chains, options: { shimDisconnect: true } }),
-//     new CoinbaseWalletConnector({
-//       chains,
-//       options: { appName: metadata.name },
-//     }),
-//   ],
-//   publicClient,
-//   webSocketPublicClient,
-// });
 
 createAppKit({
   adapters: [wagmiAdapter],
@@ -69,18 +28,6 @@ createAppKit({
     analytics: true,
   },
 });
-
-// createWeb3Modal({
-//   wagmiConfig,
-//   projectId,
-//   chains,
-//   themeVariables: {
-//     "--w3m-accent": "#fff",
-//     "--w3m-color-mix": "rgb(0,0,0,0.8)",
-//     "--w3m-color-mix-strength": 25,
-//     "--w3m-border-radius-master": "1px",
-//   },
-// });
 
 export const Web3Provider = (
   props: PropsWithChildren<Record<string, never>>
