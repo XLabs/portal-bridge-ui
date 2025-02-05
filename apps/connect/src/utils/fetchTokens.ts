8785ca04b6d9dcdaba31eda3db7a73b4dc75282f
Valentino Conti <41705567+valentinoConti@users.noreply.github.com>
2025-02-05 13:49:10 -0300

    Dynamic NTT and token config files via JSON (#1278)
    * Dynamic ntt routes via json
    
    * comment deleted
    
    * All tokens config into jsons
    
    * remove jsons from main branch
    
    * env
    
    * Add dynamic JSON 2
    
    * stupid rule
    
    * different files for prod and previews
    
    * no comments
    
    * PR Feedback

---
+++ apps/connect/src/utils/fetchTokens.ts
@@ -0,0 +1,64 @@
+import { isProduction } from "./constants";
+
+const NTT_TOKENS_URL = {
+  Mainnet: `https://raw.githubusercontent.com/XLabs/portal-bridge-ui/refs/heads/${isProduction ? "config" : "config-preview"}/tokens.ntt.mainnet.json`,
+  Testnet: `https://raw.githubusercontent.com/XLabs/portal-bridge-ui/refs/heads/${isProduction ? "config" : "config-preview"}/tokens.ntt.testnet.json`,
+};
+
+const WRAPPED_TOKENS_URL = {
+  Mainnet: `https://raw.githubusercontent.com/XLabs/portal-bridge-ui/refs/heads/${isProduction ? "config" : "config-preview"}/tokens.wrapped.mainnet.json`,
+  Testnet: `https://raw.githubusercontent.com/XLabs/portal-bridge-ui/refs/heads/${isProduction ? "config" : "config-preview"}/tokens.wrapped.testnet.json`,
+};
+
+const TOKENS_CONFIG_URL = {
+  Mainnet: `https://raw.githubusercontent.com/XLabs/portal-bridge-ui/refs/heads/${isProduction ? "config" : "config-preview"}/tokens.config.mainnet.json`,
+  Testnet: `https://raw.githubusercontent.com/XLabs/portal-bridge-ui/refs/heads/${isProduction ? "config" : "config-preview"}/tokens.config.testnet.json`,
+};
+
+export async function fetchTokensConfig(env: "Mainnet" | "Testnet") {
+  let nttTokensConfig = null;
+  let wrappedTokensConfig = null;
+  let tokensConfig = null;
+
+  try {
+    const response = await fetch(NTT_TOKENS_URL[env]);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    nttTokensConfig = data;
+  } catch (error) {
+    console.error("Failed to fetch NTT tokens config:", error);
+  }
+
+  try {
+    const response = await fetch(WRAPPED_TOKENS_URL[env]);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    wrappedTokensConfig = data;
+  } catch (error) {
+    console.error("Failed to fetch wrapped tokens config:", error);
+  }
+
+  try {
+    const response = await fetch(TOKENS_CONFIG_URL[env]);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    tokensConfig = data;
+  } catch (error) {
+    console.error("Failed to fetch tokens config:", error);
+  }
+
+  return {
+    nttTokensConfig,
+    wrappedTokensConfig,
+    tokensConfig,
+  };
+}
