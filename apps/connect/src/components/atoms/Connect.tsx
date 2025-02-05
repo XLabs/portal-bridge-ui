189f1f84c0a3898aa7164941671ea2af6a78399f
Valentino Conti <valeen.conti@hotmail.com>
2025-02-05 09:08:19 -0300

    Add dynamic JSON 2

--- apps/connect/src/components/atoms/Connect.tsx
+++ apps/connect/src/components/atoms/Connect.tsx
@@ -1,10 +1,14 @@
-import { memo, useEffect } from "react";
-import WormholeConnect from "@wormhole-foundation/wormhole-connect";
+import { memo, useEffect, useState } from "react";
+import WormholeConnect, {
+  nttRoutes,
+  WormholeConnectConfig,
+} from "@wormhole-foundation/wormhole-connect";
 import { useConnectConfig } from "../../hooks/useConnectConfig";
 import { styled } from "@mui/material";
 import { NAVBAR_WIDTH } from "./NavBar";
 import { theme } from "../../theme/connect";
 import { Banner } from "./Banner";
+import { fetchTokensConfig } from "../../utils/fetchTokens";
 
 export const Container = styled("div")(({ theme }) => ({
   paddingRight: `${NAVBAR_WIDTH}px`,
@@ -14,21 +18,62 @@ export const Container = styled("div")(({ theme }) => ({
 }));
 
 export const Connect = memo(() => {
-  const config = useConnectConfig();
+  const [isLoading, setIsLoading] = useState(true);
+  const [config, setConfig] = useState<WormholeConnectConfig | null>(null);
+
+  const offlineConfig = useConnectConfig();
 
   useEffect(() => {
-    if (config) {
-      localStorage.setItem(
-        `${window.location.href}?${import.meta.env.VITE_APP_VERSION}`,
-        JSON.stringify(config, null, 2)
-      );
+    console.log("offline", offlineConfig);
+
+    if (offlineConfig) {
+      const asyncConfig = async () => {
+        const { nttTokensConfig, tokensConfig, wrappedTokensConfig } =
+          await fetchTokensConfig("Mainnet");
+
+        const nttRoutesConfig = nttTokensConfig
+          ? nttRoutes({ tokens: nttTokensConfig })
+          : [];
+
+        const allTokensConfig: any = {};
+        if (wrappedTokensConfig) {
+          allTokensConfig.wrappedTokens = wrappedTokensConfig;
+        }
+        if (tokensConfig) {
+          allTokensConfig.tokensConfig = tokensConfig;
+        }
+
+        const fullConfig = {
+          ...offlineConfig,
+          routes: [...(offlineConfig.routes || []), ...nttRoutesConfig],
+          ...allTokensConfig,
+        };
+
+        setConfig(fullConfig);
+        console.log("fullConfig", fullConfig);
+
+        localStorage.setItem(
+          `${window.location.href}?${import.meta.env.VITE_APP_VERSION}`,
+          JSON.stringify(fullConfig, null, 2)
+        );
+
+        setIsLoading(false);
+      };
+
+      asyncConfig();
     }
-  }, [config]);
+  }, [offlineConfig]);
 
   return (
     <Container>
-      {!!config && <WormholeConnect config={config} theme={theme} />}
-      <Banner />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {!!config && <WormholeConnect config={config} theme={theme} />}
+          <Banner />
+        </>
+      )}
     </Container>
   );
 });
