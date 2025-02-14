730d029d19f27f3b24bc1810e7ce118420327304
yuli-ferna <yuliferna123@gmail.com>
2025-02-14 15:33:43 -0400

    Fixes

--- apps/rewards-dashboard/src/constants.ts
+++ apps/rewards-dashboard/src/constants.ts
@@ -5,3 +5,7 @@ export const WAC_URL_TEMPLATE = import.meta.env.VITE_WAC_URL_TEMPLATE
   export const getWACUrl = (custom: string) => {
     return WAC_URL_TEMPLATE.replace("*", custom);
   }
+
+  export const WAC_URL = import.meta.env.VITE_WAC_URL
+  ? import.meta.env.VITE_WAC_URL
+  : "https://wac.gfx.xyz";
