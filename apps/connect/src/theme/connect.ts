da2428826a23b066b567e21b0a779fd72f4f5cf4
Giuliano Conti <giuliconti1@gmail.com>
2025-02-26 18:13:55 -0300

    Connect version 2.2.0 beta 0 (#1294)
    * Connect version 2.2.0 beta 0
    
    * badge
    
    * Add Mayan SHUTTLE
    
    * Update
    
    ---------
    
    Co-authored-by: yuli-ferna <yuliferna123@gmail.com>

--- apps/connect/src/theme/connect.ts
+++ apps/connect/src/theme/connect.ts
@@ -2,5 +2,5 @@ import type { WormholeConnectTheme } from "@wormhole-foundation/wormhole-connect
 
 export const theme: WormholeConnectTheme = {
   mode: "dark",
-  badge: "rgb(15, 9, 44)",
+  // badge: "rgb(15, 9, 44)",
 };
